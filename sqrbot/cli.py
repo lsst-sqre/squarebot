7bde5700ec08fdaaefc89472c0681c3bedafd5d4
Jonathan Sick <jsick@lsst.org>
2018-12-26 23:47:55 -0700

    Add sqrbot run command
    This command uses the run_app function to run the app itself in
    localhost and on a given port. The Docker container will use this
    command to run the app so that nginx can proxy it.

--- sqrbot/cli.py
+++ sqrbot/cli.py
@@ -1,10 +1,12 @@
 """Administrative command line interface for sqrbot.
 """
 
-__all__ = ('main', 'help')
+__all__ = ('main', 'help', 'run')
 
+from aiohttp.web import run_app
 import click
 
+from sqrbot.app import create_app
 
 # Add -h as a help shortcut option
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -35,3 +37,16 @@ def help(ctx, topic, **kw):
         click.echo(ctx.parent.get_help())
     else:
         click.echo(main.commands[topic].get_help(ctx))
+
+
+@main.command()
+@click.option(
+    '--port', default=8080, type=int,
+    help='Port to run the application on.'
+)
+@click.pass_context
+def run(ctx, port):
+    """Run the application (for production).
+    """
+    app = create_app()
+    run_app(app, port=port)
