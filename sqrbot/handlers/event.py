2934b01e106fd7a8ed13a184c62cb2c1bb8d78f9
Jonathan Sick <jsick@lsst.org>
2019-01-02 14:20:34 -0500

    Use signing secret to verify Slack request
    This adds a _verify_request function that implements the signing secret
    algorithm to verify the authenticity of a Slack request. This is more
    secure than the token method.
    
    The implementation is based on the docs at
    https://api.slack.com/docs/verifying-requests-from-slack as well as the
    Flask-based library
    https://github.com/slackapi/python-slack-events-api/blob/master/slackeventsapi/server.py
    This implementation is simplified since it's Python 3-only.
    
    Since _verify_request is called at the top of the handler, other code
    such as the challenge handler not longer need to worry about verify
    tokens.

--- sqrbot/handlers/event.py
+++ sqrbot/handlers/event.py
@@ -1,5 +1,10 @@
 __all__ = ('post_event',)
 
+import hmac
+import hashlib
+import math
+import time
+
 from aiohttp import web
 from sqrbot.routes import routes
 
@@ -9,6 +14,9 @@ async def post_event(request):
     """Handle an event post by the Slack Events API.
     """
     logger = request['logger']
+    # Verify the Slack signing secret on the request
+    await _verify_request(request)
+
     slack_event = await request.json()
     logger.info('Got event', payload=slack_event)
 
@@ -21,18 +29,50 @@ async def post_event(request):
 def _handle_url_verification(request, slack_event):
     """Handle an Events API ``url_verification`` request with a ``challenge``
     field to verify the app.
-    """
-    expected_token = request.config_dict['sqrbot-jr/slackVerificationToken']
-    try:
-        if slack_event['token'] != expected_token:
-            return web.json_response(
-                status=401,
-                reason='Verification token does not match')
-    except KeyError:
-        return web.json_response(
-            status=400,
-            reason='"token" missing from request body')
 
+    Notes
+    -----
+    This assumes that the request's ``type`` is ``url_verification`` and that
+    the request has already been verified.
+    """
     return web.json_response(
         {'challenge': slack_event['challenge']},
         status=200)
+
+
+async def _verify_request(request):
+    """Verify the authenticity of a request from Slack using the signing
+    secret method.
+
+    See: https://api.slack.com/docs/verifying-requests-from-slack
+    """
+    timestamp = request.headers['X-Slack-Request-Timestamp']
+
+    if math.fabs(time.time() - float(timestamp)) > 300.:
+        # The request timestamp is more than five minutes from local time.
+        # It could be a replay attack, so let's ignore it.
+        raise web.HTTPException(
+            status=400,
+            reason='X-Slack-Request-Timestamp is older than 5 minutes.'
+        )
+
+    # Ensure that no special decoding is done on the body
+    body_bytes = await request.read()
+    body = body_bytes.decode(encoding='utf-8')
+
+    signing_secret = request.config_dict['sqrbot-jr/slackSigningSecret']
+
+    base_signature = f'v0:{timestamp}:{body}'
+    signature_hash = 'v0=' + hmac.new(
+        signing_secret.encode(),
+        msg=base_signature.encode(),
+        digestmod=hashlib.sha256).hexdigest()
+
+    if hmac.compare_digest(
+            signature_hash, request.headers['X-Slack-Signature']):
+        return True
+    else:
+        raise web.HTTPException(
+            status=400,
+            reason='Could not successfully verify X-Slack-Signature'
+        )
