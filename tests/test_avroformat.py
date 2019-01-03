6adf4031652dbec0bc8dd44e2fbbae77cce7af17
Jonathan Sick <jsick@lsst.org>
2019-01-03 16:17:13 -0500

    Test encoding different types of messages
    The encode_slack_message function looks up the schema appropriate for
    the message type, and returns Avro-encoded bytes.
    
    The primary application for this is to have tests for encoding messages.
    
    To test this, test_encode_message() uses several example messages from
    the Slack docs (e.g. https://api.slack.com/events/message.channels) that
    are included here in the tests/slack_messages/ directory.

--- tests/test_avroformat.py
+++ tests/test_avroformat.py
@@ -1,15 +1,21 @@
 """Tests for the sqrbot.avroformat module.
 """
 
-import pytest
+from io import BytesIO
+import json
+from pathlib import Path
 
-from sqrbot.avroformat import load_event_schema, validate_avro_schema
+import pytest
+import fastavro
+
+from sqrbot.avroformat import (load_event_schema, validate_avro_schema,
+                               encode_slack_message)
 
 
 @pytest.mark.parametrize(
     'event_type',
     [
-        'message.im'
+        'message'
     ]
 )
 def test_load_event_schema(event_type):
@@ -20,3 +26,32 @@ def test_load_event_schema(event_type):
     validate_avro_schema(schema)
 
     assert schema['name'] == event_type
+
+
+@pytest.mark.parametrize(
+    'message_filename',
+    [
+        'message.im.json',
+        'message.channels.json',
+        'message.groups.json',
+        'message.mpim.json',
+    ]
+)
+def test_encode_message(message_filename):
+    p = Path(__file__).parent / 'slack_messages' / 'message.im.json'
+    message = json.loads(p.read_text())
+    data = encode_slack_message(message)
+    assert isinstance(data, bytes)
+
+    # Try to read the message back
+    event_type = message['event']['type']
+    schema = load_event_schema(event_type)
+    bytes_fh = BytesIO()
+    bytes_fh.write(data)
+    bytes_fh.seek(0)
+    decoded_message = fastavro.schemaless_reader(bytes_fh, schema)
+    # messages are not equal because the original data may have more fields
+    # than the original message, or the avro-encoded message may have defaults
+    # added to it.
+    # assert decoded_message == message
+    assert 'event' in decoded_message
