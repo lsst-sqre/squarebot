25d734323b8fd9e2946ad18d32e0f0fa9142af38
Jonathan Sick <jsick@lsst.org>
2023-06-05 14:48:21 -0400

    Integrate redoc into the build
    This uses the default sphinxcontrib-redoc flow, creating a rest.html
    page using the openapi.json spec created by documenteer.ext.openapi.

--- docs/conf.py
+++ docs/conf.py
@@ -4,3 +4,17 @@ from documenteer.conf.guide import *
 documenteer_openapi_generator = {
     "func": "squarebot.main:create_openapi",
 }
+
+redoc = [
+    {
+        "name": "REST API",
+        "page": "rest",
+        "spec": "_static/openapi.json",
+        "embed": True,
+        "opts": {"hide-hostname": True},
+    }
+]
+
+redoc_uri = (
+    "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"
+)
