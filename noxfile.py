73ed9f4b57818b1a8eebf4661258744424c6e8d5
Jonathan Sick <jsick@lsst.org>
2023-06-09 14:08:21 -0400

    Add nox test session
    This session runs pytest on server/tests.

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 # Default sessions
-nox.options.sessions = ["lint", "typing"]
+nox.options.sessions = ["lint", "typing", "test"]
 
 # Other nox defaults
 nox.options.default_venv_backend = "venv"
@@ -31,3 +31,25 @@ def typing(session):
         session.run("mypy", "src", "tests", *session.posargs)
     with session.chdir("client"):
         session.run("mypy", "src", *session.posargs)
+
+
+@nox.session
+def test(session):
+    _install(session)
+    with session.chdir("server"):
+        session.run(
+            "pytest",
+            "--cov=squarebot",
+            "--cov-branch",
+            *session.posargs,
+            env={
+                "SAFIR_LOG_LEVEL": "DEBUG",
+                "SAFIR_ENVIRONMENT_URL": "http://example.org",
+                "SQUAREBOT_REGISTRY_URL": "http://registry.example.org",
+                "KAFKA_BOOTSTRAP_SERVERS": "http://broker.example.org",
+                "KAFKA_SECURITY_PROTOCOL": "PLAINTEXT",
+                "SQUAREBOT_SLACK_SIGNING": "1234",
+                "SQUAREBOT_SLACK_TOKEN": "1234",
+                "SQUAREBOT_SLACK_APP_ID": "1234",
+            },
+        )
