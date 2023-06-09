2babe1ff2922c33cfa25c92877cb8f5bf42d5752
Jonathan Sick <jsick@lsst.org>
2023-06-09 15:05:12 -0400

    Add nox session for building the docs

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 # Default sessions
-nox.options.sessions = ["lint", "typing", "test"]
+nox.options.sessions = ["lint", "typing", "test", "docs"]
 
 # Other nox defaults
 nox.options.default_venv_backend = "venv"
@@ -15,6 +15,22 @@ def _install(session, *args, **kwargs):
     session.install("-r", "server/requirements/dev.txt")
     session.install("-e", "client")
     session.install("-e", "server")
+
+
+def _make_env_vars():
+    """Create a environment variable dictionary for test sessions that enables
+    the app to start up.
+    """
+    return {
+        "SAFIR_LOG_LEVEL": "DEBUG",
+        "SAFIR_ENVIRONMENT_URL": "http://example.org",
+        "SQUAREBOT_REGISTRY_URL": "http://registry.example.org",
+        "KAFKA_BOOTSTRAP_SERVERS": "http://broker.example.org",
+        "KAFKA_SECURITY_PROTOCOL": "PLAINTEXT",
+        "SQUAREBOT_SLACK_SIGNING": "1234",
+        "SQUAREBOT_SLACK_TOKEN": "1234",
+        "SQUAREBOT_SLACK_APP_ID": "1234",
+    }
 
 
 @nox.session
@@ -42,14 +58,25 @@ def test(session):
             "--cov=squarebot",
             "--cov-branch",
             *session.posargs,
-            env={
-                "SAFIR_LOG_LEVEL": "DEBUG",
-                "SAFIR_ENVIRONMENT_URL": "http://example.org",
-                "SQUAREBOT_REGISTRY_URL": "http://registry.example.org",
-                "KAFKA_BOOTSTRAP_SERVERS": "http://broker.example.org",
-                "KAFKA_SECURITY_PROTOCOL": "PLAINTEXT",
-                "SQUAREBOT_SLACK_SIGNING": "1234",
-                "SQUAREBOT_SLACK_TOKEN": "1234",
-                "SQUAREBOT_SLACK_APP_ID": "1234",
-            },
+            env=_make_env_vars(),
+        )
+
+
+@nox.session
+def docs(session):
+    _install(session)
+    doctree_dir = (session.cache_dir / "doctrees").absolute()
+    with session.chdir("docs"):
+        session.run(
+            "sphinx-build",
+            "-W",
+            "--keep-going",
+            "-n",
+            "-T",
+            "-b" "html",
+            "-d",
+            str(doctree_dir),
+            ".",
+            "./_build/html",
+            env=_make_env_vars(),
         )
